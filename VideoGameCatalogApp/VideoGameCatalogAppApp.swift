218ca352ffddeb9f0eded042044a2314c6eb4ec4
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-10-10 04:42:51 -0500

    Initial Commit

---
+++ VideoGameCatalogApp/VideoGameCatalogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoGameCatalogAppApp.swift
+//  VideoGameCatalogApp
+//
+//  Created by brian waobikeze on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoGameCatalogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
